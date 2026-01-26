8ece2f64c5be75b4716466aaedfd891c03e7d176
Sagarkumar6204 <bittukumar9006716@gmail.com>
2026-01-26 16:16:22 +0530

    nav and footer added

--- src/OpenAccount.js
+++ src/OpenAccount.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 function OpenAccount() {
   return (
-    <div>
-      OpenAccount
+    <div className="container p-3 mt-0">
+      <div className="row text-center">
+        <div className="row">
+         
+          <h1 className="mt-0">Open Zerodha Account</h1>
+          <p>
+            Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.
+          </p>
+          <button style={{width:"13rem",margin:"0 auto"}} className="p-2 btn btn-primary fs-5 mb-5 text-nowrap">Sign up Now</button>
+        </div>
+      </div>
     </div>
   )
 }
